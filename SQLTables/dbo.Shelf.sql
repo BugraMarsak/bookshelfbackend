0bb1dd85b86ab91f5099e5c57af1170f4b70535b
Buğra MARSAK <47815793+BugraMarsak@users.noreply.github.com>
2021-12-28 14:38:24 +0300

    Add files via upload

---
+++ SQLTables/dbo.Shelf.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Shelf] (
+    [ShelfId] INT IDENTITY (1, 1) NOT NULL,
+    [UserId]  INT NOT NULL,
+    [BookId]  INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([ShelfId] ASC)
+);
+
---
+++ SQLTables/dbo.UserOperationClaims.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[UserOperationClaims] (
+    [Id]               INT          IDENTITY (1, 1) NOT NULL,
+    [UserId]           VARCHAR (50) NOT NULL,
+    [OperationClaimId] VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
